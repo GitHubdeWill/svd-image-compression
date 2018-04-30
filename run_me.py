bbde47deea02a787720a67aaf1e6623abcbdec68
Yi Fung <yirenfung@gmail.com>
2018-04-29 22:23:29 -0400

    I'm adding the read_faces_rgb() function, as well as comments, here

--- run_me.py
+++ run_me.py
@@ -1,23 +1,71 @@
+########################################################################
+#
+#  Math 545 Final Project
+#
+#  Professor: Qian-Yong Chen
+#  Students: Yi Fung, William He
+#
+#  Due Date: April 30, 2018
+#
+#  Note: this is running on python v3
+#
+#  Thank you for grading and have a great summer!
+#
+########################################################################
+
+
+
+### Import libraries at the top ###
 import numpy as np
 from scipy import misc
 import imageio
+from PIL import Image
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
 
 def read_faces():
-	nFaces = 21
-	nDims = 10000
+	'''
+	Helper function to read in the image data
+
+	Input: None
+	Output: data_x - a 20 x 10000 matrix
+	'''
+	nFaces = 20+1
+	nDims = 100*100
 	data_x = np.empty((0, nDims), dtype=float)
 	for i in np.arange(1, nFaces):
-		#data_x = np.vstack((data_x, np.reshape(misc.imread('../../Data/Faces/face_%s.png' % (i)), (1, nDims))))
 		data_x = np.vstack((data_x, np.reshape(imageio.imread('./pics/%s.jpeg' % (i)), (1, nDims))))
 	return (data_x)
 
 
+def read_faces_rgb():
+	'''
+	Helper function to read in the image data in colored format
+
+	Input: None
+	Output: data_x for R, data_x for G, data_x for B
+	'''
+	nFaces = 20
+	nDims = 100*100
+	data_x_r = np.empty((0, nDims), dtype=float)
+	data_x_g = np.empty((0, nDims), dtype=float)
+	data_x_b = np.empty((0, nDims), dtype=float)
+	for i in np.arange(1, nFaces):
+		im = Image.open('./pics/%s.jpeg' % (i)).convert('RGB')
+		pix = np.array(im)
+		data_x_r = np.vstack((data_x_r, np.reshape(pix[:,:,1], (1,nDims))))
+		data_x_g = np.vstack((data_x_g, np.reshape(pix[:,:,2], (1,nDims))))
+		data_x_b = np.vstack((data_x_b, np.reshape(pix[:,:,3], (1,nDims))))
+	return (data_x_r, data_x_g, data_x_b)
+
+
 def helper_plot_grid(cache, title, c=None):
 	'''
-	Helper function for plotting 3x3 grid, image visualization 
+	Helper function for plotting 3x3 grid, image visualization
+
+	Input: cache data of one the images using PCA w/ different # of components
+	Output: Outputs the 3x3 plot and returns nothing 
 	'''
 	f, axarr = plt.subplots(3, 3)
 	axarr[0, 0].imshow(cache[0][0], cmap=c)
@@ -50,15 +98,16 @@ def helper_plot_grid(cache, title, c=None):
 
 if __name__ == '__main__':
 
-	################################################
-	# PCA
+	########## This is the driver and main function ############
+	print('Running PCA...')
 
 	data_x = read_faces()
-	print('X = ', data_x.shape)
+	print('Size of X = ', data_x.shape)
 
-	print('Implement PCA here ...')
 	U, s, V = np.linalg.svd(data_x)
-	print(V.shape)
+	#print('Size of U = ', U.shape)
+	#print('Size of s = ', s.shape)
+	#print('Size of V = ', V.shape)
 
 	cache = [(data_x[1].reshape(100,100), "Original")]
 	for i in [3,5,10,25,50,100,150,200]:
@@ -67,9 +116,9 @@ if __name__ == '__main__':
 		X_projected = np.dot(data_x, w_k.T)
 		X_recon = np.dot(X_projected, w_k)
 		print("Reconstruction error: " + str(np.sqrt(np.mean((data_x-X_recon)**2))))
-		print(X_projected.nbytes)
-		print(w_k.nbytes)
-		print(data_x.nbytes)
+		#print("# bytes in X_projected: " + str(X_projected.nbytes))
+		#print("# bytes in w_k: " + str(w_k.nbytes))
+		#print("# bytes in data_x: " + str(data_x.nbytes))
 		print("Compression rate: " + str(float(X_projected.nbytes + w_k.nbytes)/float(data_x.nbytes)))
 		cache.append((X_recon[1].reshape(100,100), "k=" + str(i)))
 	helper_plot_grid(cache, "PCA reconstruction", "gray")
