7cabd7b9cd5397756ec731f93416bcb02f219354
GitHubdeWill <hd209458inj@gmail.com>
2018-04-29 23:04:01 -0400

    convert pics as original resize to 100*100 for driver

--- driver.py
+++ driver.py
@@ -14,26 +14,26 @@ import imgutils
 def process(filename):
 
     img = cv2.imread(filename)
-    img = imgutils.forceGrey(img)
+    # img = imgutils.forceGrey(img)
 
     img = cv2.resize(img, (100, 100))
     # yeah...
     data_x = img
-    print('X = ', data_x.shape)
-
-    print('Implement PCA here ...')
-    U, s, V = np.linalg.svd(data_x)
-
-    cache = [(data_x.reshape(100, 100), "Original")]
-    for i in [3, 5, 10, 30, 50, 100, 150, 300]:
-        print("k = " + str(i))
-        w_k = V[:i]
-        X_projected = np.dot(data_x, w_k.T)
-        X_recon = np.dot(X_projected, w_k)
-        print("Reconstruction error: " + str(np.sqrt(np.mean((data_x-X_recon)**2))))
-        print("Compression rate: " + str((X_projected.nbytes + w_k.nbytes)/data_x.nbytes))
-        cache.append((X_recon[1].reshape(100, 100), "k=" + str(i)))
-    imgutils.helper_plot_grid(cache, "PCA reconstruction", "gray")
+    # print('X = ', data_x.shape)
+    #
+    # print('Implement PCA here ...')
+    # U, s, V = np.linalg.svd(data_x)
+    #
+    # cache = [(data_x.reshape(100, 100), "Original")]
+    # for i in [3, 5, 10, 30, 50, 100, 150, 300]:
+    #     print("k = " + str(i))
+    #     w_k = V[:i]
+    #     X_projected = np.dot(data_x, w_k.T)
+    #     X_recon = np.dot(X_projected, w_k)
+    #     print("Reconstruction error: " + str(np.sqrt(np.mean((data_x-X_recon)**2))))
+    #     print("Compression rate: " + str((X_projected.nbytes + w_k.nbytes)/data_x.nbytes))
+    #     cache.append((X_recon[1].reshape(100, 100), "k=" + str(i)))
+    # imgutils.helper_plot_grid(cache, "PCA reconstruction", "gray")
 
     return img
 
@@ -54,20 +54,20 @@ def main(name):
         filelist = os.listdir(name)
 
         for file in filelist:
-            print file
+            print(file)
 
             if file.split('.')[-1] == 'png' or \
                file.split('.')[-1] == 'jpg' or \
                file.split('.')[-1] == 'jpeg':
                 c += 1
-                print c
+                print(c)
 
                 key = raw_input("Press any other key to skip, ENTER to continue: ")
                 if len(key) > 0:
                     continue
 
-                process(name+file)
-                # cv2.imwrite(name+str(c)+"."+file.split(".")[-1], im)
+                im = process(name+file)
+                cv2.imwrite(name+str(c)+"RGB."+file.split(".")[-1], im)
 
                 print()  # Newline
                 raw_input("press any key to continue.")
